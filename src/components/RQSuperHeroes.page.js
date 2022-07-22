2fdef08b5f2f637d7650b0b9efe00582d694cd99
Inokcode <regilearn@gmail.com>
2022-07-22 19:02:29 +0530

    bug fix

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,9 +6,16 @@ const fetchSuperHeroes = () => {
 };
 
 const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    ["super-heroes"],
+    fetchSuperHeroes
+  );
+  console.log({ error });
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
   return (
     <div>
